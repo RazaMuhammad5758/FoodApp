00a12c37f6690d873989bf8a874f1f06373ec8f8
RazaMuhammad5758 <waistband161@gmail.com>
2025-07-10 13:54:31 +0500

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Normal Bootstrap
+import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css'; // Dark Mode CSS
+import Home from './screens/Home';
+
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <div>
+        <Home/>
+      </div>
+    
+    </>
   );
 }
 
